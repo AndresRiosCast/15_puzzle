a29dd6f1aa40805945813820ca793cc64739a5ec
unknown <Andres Rios>
2025-10-10 15:43:06 -0500

    Se cambiaron el nombre de funciones y variables para mejorar el entendiemiento del programa, tambien se agrego la funcion de movilidad lo que permite hacer una prueba de juego en consola

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,6 @@
-def crearmatrix (tamaño):
-    print (tamaño)
-    matrix = []
+import random
+def crear_matriz (tamaño):
+    matriz = []
     contador = 0
     for i in range(tamaño):
         filas = []  # Crear nueva lista para cada fila
@@ -10,13 +10,44 @@ def crearmatrix (tamaño):
             else:
                 contador +=1
                 filas.append(contador)            
-        matrix.append(filas)
-    return matrix
+        matriz.append(filas)
+
+    matriz_solucion = [fila[:] for fila in matriz]
+
+    for i in matriz:
+        random.shuffle(i)
+    random.shuffle(matriz)
+    
+    return matriz,matriz_solucion
 
 def mostrar_matriz(matriz):
     for i in matriz:
         print(i)
 
+def movilidad(matriz,movimiento):
+    for i in range(len(matriz)):
+        for u in range(len(matriz[i])):
+            if matriz[i][u] == 0:
+                match movimiento:
+                    case "arriba":
+                        if i-1>-1:
+                            matriz[i][u],matriz[i-1][u]=matriz[i-1][u], matriz[i][u]
+                    case "abajo":
+                        if i+1<(len(matriz)):
+                            matriz[i][u],matriz[i+1][u]=matriz[i+1][u], matriz[i][u]
+                    case "derecha":
+                        if u+1<(len(matriz)):
+                            matriz[i][u],matriz[i][u+1]=matriz[i][u+1], matriz[i][u]
+                    case "izquierda":
+                        if u-1>-1:
+                            matriz[i][u],matriz[i][u-1]=matriz[i][u-1], matriz[i][u]                           
+                return matriz
+
 tamaño = int(input("Ingrese el tamaño que desea: "))
-mostrar_matriz(crearmatrix(tamaño))
+matriz,matriz_solucion = crear_matriz(tamaño)
+mostrar_matriz(matriz)
+
+while matriz != matriz_solucion:
+    movimiento=str(input("Ingrese un movimiento (arriba,abajo,derecha,izquierda): "))
+    mostrar_matriz(movilidad(matriz,movimiento))
 
